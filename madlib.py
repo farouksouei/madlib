66db3ccf71ba816714847f1b9212ede484afa7f1
Mohamed Farouk Souei <farouksouei@gmail.com>
2022-01-20 14:02:36 +0100

    first commit

---
+++ madlib.py
@@ -0,0 +1,13 @@
+from msilib.schema import Verb
+
+
+adjective = input("Enter an adjective: ")
+Verb1 = input("Enter a verb: ")
+Verb2 = input("Enter a verb: ")
+famousPerson = input("Enter a famous person: ")
+
+madlib = f"Computer programming is so {adjective} that it can be used to \n make {famousPerson} write {Verb1} and {Verb2}."
+madlib2 = f"Skyding is so {adjective} that it can be used to make {famousPerson} write {Verb1} and {Verb2}."
+
+print(madlib)
+print(madlib2)
